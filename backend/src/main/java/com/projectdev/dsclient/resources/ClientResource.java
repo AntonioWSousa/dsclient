e6ff8017f8c82747033173b67ac9014ff56f4465
Antonio Sousa <antoniowsj@outlook.com>
2021-09-25 13:55:38 -0300

    ClientResource

---
+++ backend/src/main/java/com/projectdev/dsclient/resources/ClientResource.java
@@ -0,0 +1,27 @@
+package com.projectdev.dsclient.resources;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projectdev.dsclient.entities.Client;
+
+@RestController
+@RequestMapping(value = "/clients")
+public class ClientResource {
+	
+	@GetMapping
+	public ResponseEntity<List<Client>> findAll() {
+		List<Client> list = new ArrayList<>();
+		list.add(new Client(1L, "Danilo Ferreira", "77810690231", 7000.0, Instant.now() , 0));
+		return ResponseEntity.ok().body(list);
+		
+		
+	}
+
+}
